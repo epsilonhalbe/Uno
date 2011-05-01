766b639f78acf172e79c879318fdc369efaee048
Martin Heuschober <epsilonhalbe@gmail.com>
2011-05-02 01:57:56 +0200

    almost ready - remains to safeguard the card-drawing-phase and write some tests

--- Uno.hs
+++ Uno.hs
@@ -4,7 +4,7 @@ import Common
 import System.Random (getStdRandom, randomR)
 import System.Console.ANSI (setSGR,SGR(SetColor),ColorIntensity(Vivid),ConsoleLayer(Foreground),Color(Black, Red, Green, Yellow, Blue, Magenta, Cyan, White))
 import Data.List (elemIndex)
-import System.IO (hSetBuffering, stdin, BufferMode(NoBuffering,LineBuffering))
+import Control.Applicative ((<$>))
 
 import System.Console.ANSI (Color(Black, Red, Green, Yellow, Blue, Magenta, Cyan, White))
 -- additional instances for the Color datatype
@@ -15,74 +15,51 @@ main = do
         blacks = [Card Black v | v <- [Plus4,ChCol]]
         full_deck = (zeroes ++ (nplicate 2 ncards) ++ (nplicate 4 blacks))
     putStrLn "how many players do play this game?"
-    hSetBuffering stdin NoBuffering
-    c <- getChar
-    num_of_players <- return (read [c] :: Int) -- todo watch out for hflush errors ++make safe++
+    num_of_players <- safeGetInt
     (rest,players) <- get_Players_Deck num_of_players full_deck
     n <- getStdRandom (randomR (0,(length players) - 1))
     (deck, d_stack) <- drawCards 1 rest
     let players' = (drop n players) ++ (take n players)
     putStrLn ((show d_stack) ++ (show players'))
-    game_loop (State players' deck d_stack)
+    new_game_loop (State players' deck d_stack)
 
-game_loop :: State -> IO ()
-game_loop (State (player:others) deck d_stack) = do
+new_game_loop :: State -> IO ()
+new_game_loop state = do
     let topcard = head d_stack
-    displayC topcard
+    putStr "The top card on the Stack: "
+    displayCs d_stack
+    -- displayC topcard
     putStrLn "\n\n\n\n"
-{- spaghetti code at its best -jibberjabber-}
-    case (value topcard) of
-         Stop  -> do
-             putStrLn "Stop - Hammertime"
-             game_loop (State (others++[player]) deck ([Card (color topcard) Dummy] ++ d_stack))
-         Plus2 -> do
-             putStrLn "Nimm 2 denn naschen ist gesund"
-             (tdeck, pluscards) <- drawCards 2 deck
-             let tplayer = updatePlayer player ((hand player) ++ pluscards)
-             displayCs (hand tplayer)
-             putStrLn (name tplayer)
-             -- putStrLn $ show (hand player)
-             state' <- putCard_Phase (State (tplayer:others) tdeck d_stack)
-             game_loop state'
-         Plus4 -> do
-             putStrLn "ohooo nimm' doch 4 karten"
-             (tdeck, pluscards) <- drawCards 4 deck
-             let tplayer = updatePlayer player ((hand player) ++ pluscards)
-             displayCs (hand tplayer)
-             putStrLn (name tplayer)
-             -- putStrLn $ show (hand player)
-             (State players' deck' td_stack) <- putCard_Phase (State (tplayer:others) tdeck d_stack)
-             game_loop (State players' deck' (remove_fake td_stack))
-             -- memo special handling for black cards
-         Dummy -> do
-             displayCs (hand player)
-             putStrLn (name player)
-             -- putStrLn $ show (hand player)
-             (State players' deck' td_stack) <- putCard_Phase (State (player:others) deck d_stack)
-             let state' =  State players' deck' (remove_fake td_stack)
-             win (last players') (game_loop state')
-             -- memo special handling for black cards
-         _ -> do
-             displayCs (hand player)
-             putStrLn (name player)
-             -- putStrLn $ show (hand player)
-             state'<- putCard_Phase (State (player:others) deck d_stack)
-             win (last $ players state') (game_loop state')
+    case v of Dummy -> do
+                        putStrLn $ "Player: " ++ (name player)
+                        displayCs (hand player)
+                        -- putStrLn $ show (hand player)
+                        (State players' deck' td_stack) <- putCard_Phase1 state
+                        let state' =  State players' deck' (remove_fake td_stack)
+                        win (last players') (new_game_loop state')
+              _ -> do
+                        putStrLn $ "Player: " ++ (name player)
+                        putStrLn "0: Take a Card"
+                        displayCs (hand player)
+                        -- putStrLn $ show (hand player)
+                        state'<- putCard_Phase1 state
+                        win (last $ players state') (new_game_loop state')
+    where
+        (State (player:others) deck d_stack) = state
+        v = value (head d_stack)
 
--- todo - make for players not only hplayers --
 get_Players_Deck :: Int -> [Card] -> IO (Deck, [Player])
 get_Players_Deck n deck = get_Players_Deck' n deck [] where
     get_Players_Deck' n deck players
         | n <= 0 = return (deck, players)
         | otherwise = do
             putStrLn ("please enter player " ++ show n ++ "'s name")
-            hSetBuffering stdin LineBuffering
             name <- getLine -- todo make safe version
             (deck', hand) <- drawCards 5 deck
             let player = HPlayer name hand
             get_Players_Deck' (n - 1) deck' ([player] ++ players)
 
-drawCards :: Int -> [Card] -> IO (Hand, Deck)
+drawCards :: Int -> [Card] -> IO (Deck, Hand)
 -- todo make safe drawing e.g. draw 4 and just 3 cards in deck
 drawCards n cards = drawCards' n (cards, []) where
     drawCards' n (cards, drawn)
@@ -91,58 +68,99 @@ drawCards n cards = drawCards' n (cards, []) where
                 (cs, c) <- getRandomCardfrom cards
                 drawCards' (n - 1) (cs,[c] ++ drawn)
 
+new_drawCards :: Int -> State -> IO (State, Hand)
+new_drawCards n state = new_drawCards' n (state, []) where
+    new_drawCards' n (state, drawn)
+        |n <= 0 = return (state, drawn)
+        |deck == [] = new_drawCards' n (State players d_stack [topcard], drawn)
+        |otherwise = new_drawCards' (n - 1) (State players cs d_stack,c:drawn)
+        where (State players deck (topcard:d_stack)) = state
+              (c:cs) = deck
+
 getRandomCardfrom :: [Card] -> IO ([Card],Card)
 getRandomCardfrom cards = do
     n <- getStdRandom (randomR (0,(length cards) - 1))
     return $ pick n cards
 
-putCard_Phase :: State -> IO State
-putCard_Phase (State (player:others) deck d_stack) = do
-    hand <- return (hand player)
+putCard_Phase1 :: State -> IO State
+putCard_Phase1 state = do
     putStrLn "which card do you put down"
-    hSetBuffering stdin NoBuffering
-    c <- getChar -- todo make safe version
-    n <- return (read [c] :: Int)
-    if n == 0 -- draw a card if entered 0
-      then do
-        (deck',hand') <- drawCards 1 deck
-        let player' = updatePlayer player (hand'++hand)
-        return (State (others++[player']) deck' d_stack)
-      else do
-        let card = last d_stack
-            pcard = (hand !! (n - 1))
-            hand' = (cancel n hand)
-        if   (color pcard == color card
-          ||  value pcard == value card
-          ||  color pcard == Black)
-          then do
-              player' <- return $ updatePlayer player hand'
-              putStrLn $ show player'
-              case pcard of (Card _ ChDir) -> do
-                                                let players' = (reverse others) ++ [player']
-                                                return $ (State players' deck ([pcard]++d_stack))
-                            (Card Black _) -> do
-                                                let players' = others ++ [player']
-                                                dummy <- getDummy pcard
-                                                return $ (State players' deck ([dummy]++[pcard]++d_stack))
-                            (Card _ _) -> do
-                                            return $ State (others ++ [player']) deck ([pcard] ++ d_stack)
-          else do
-                  putStrLn "take a valid card or take one with 0"
-                  putCard_Phase (State (player:others) deck d_stack)
+    n <- safeGetInt
+    putCard_Phase2 n state
 
+putCard_Phase2 :: Int -> State -> IO State
+putCard_Phase2 n state
+    | n == 0 = do
+        (deck',hand') <- drawCards 1 deck
+        let player' = updatePlayer player (hand'++p_hand)
+        return $ (State (others++[player']) deck' d_stack)
+    | 1 <= n && n <= (length p_hand) = putCard_Phase3 n state
+    | otherwise = do
+        putStrLn "number out of bounds - try again"
+        putCard_Phase1 state
+    where
+        State (player:others) deck d_stack = state
+        p_hand = hand player
+
+putCard_Phase3 :: Int -> State -> IO State
+putCard_Phase3 n state
+    | p_card `equiv` s_card = do
+        putCard_Phase4 p_card (State (player':others) deck d_stack)
+    | otherwise = do
+        putStrLn "put down a valid card or take one with 0"
+        putCard_Phase1 state
+    where
+        State (player:others) deck d_stack = state
+        s_card = head d_stack
+        p_hand = hand player
+        p_card = p_hand !! (n - 1)
+        new_hand = cancel n p_hand
+        player' = updatePlayer player new_hand
+
+putCard_Phase4 :: Card -> State -> IO State
+putCard_Phase4 card state
+    | v == ChDir = do
+                            let players' = (reverse others) ++ [player]
+                            return $ (State players' deck ([card]++d_stack))
+    | v == Plus2 = do
+                            (deck', pluscards) <- drawCards 2 deck
+                            let p2' = updatePlayer p2 ((hand p2) ++ pluscards)
+                            return $ (State ((p2':others') ++ [player]) deck' (card:d_stack))
+    | v == Plus4 = do
+                            (deck', pluscards) <- drawCards 4 deck
+                            let p2' = updatePlayer p2 ((hand p2) ++ pluscards)
+                            dummy <- getDummy card
+                            return $ (State ((p2':others') ++ [player]) deck' (dummy:card:d_stack))
+    | v == Stop = do
+                            let players' = (others') ++ [player,p2]
+                            return $ (State players' deck ([card]++d_stack))
+    | v == ChCol = do
+                            dummy <- getDummy card
+                            return $ (State (others ++ [player]) deck (dummy:card:d_stack))
+    | otherwise = do
+                            return $ (State (others ++ [player]) deck (card:d_stack))
+    where
+        v = value card
+        State (player:others) deck d_stack = state
+        (p2:others') = others
+
+equiv :: Card -> Card -> Bool
+equiv c1 c2
+    | color c1 == color c2 = True
+    | value c1 == value c2 = True
+    | color c1 == Black = True
+    | otherwise = False
 
 {- todo make special procedures for black cards -}
 getDummy :: Card -> IO Card
 getDummy c = do
     putStrLn "please do give me a Color"
-    putStrLn "1:Red\t2:Green\t3:Yellow\t4:Blue"
-    hSetBuffering stdin NoBuffering
-    x <- getChar -- todo make safe version
-    case x of '1' -> do return $ Card Red Dummy
-              '2' -> do return $ Card Green Dummy
-              '3' -> do return $ Card Yellow Dummy
-              '4' -> do return $ Card Blue Dummy
+    putStrLn "1:Red    2:Green    3:Yellow    4:Blue"
+    x <- safeGetInt
+    case x of 1 -> do return $ Card Red Dummy
+              2 -> do return $ Card Green Dummy
+              3 -> do return $ Card Yellow Dummy
+              4 -> do return $ Card Blue Dummy
               _ -> do getDummy c
 
 
@@ -161,18 +179,47 @@ updatePlayer (HPlayer name _) hand = HPlayer name hand
 updatePlayer (AiPlayer name _) hand = AiPlayer name hand
 
 displayCs :: [Card] -> IO ()
-displayCs [] = putStr "\n"
-displayCs (c:cs) = do
-    displayC c
-    displayCs cs
+displayCs cs = displayCs' 1 cs
+    where displayCs' n [] = putStr "\n"
+          displayCs' n (c:cs) = do
+              displayC' n c
+              displayCs' (n + 1) cs
+
+displayC' :: Int -> Card -> IO ()
+displayC' n (Card color value) = do
+    putStr $ show n ++ ":"
+    setSGR [SetColor Foreground Vivid color]
+    putStr $ "|"++show value ++"|    "++break_at_8 n
+    setSGR [SetColor Foreground Vivid White]
+    where break_at_8 n
+            |(mod n 8) == 0= "\n"
+            |otherwise = []
 
 displayC :: Card -> IO ()
 displayC (Card color value) = do
     setSGR [SetColor Foreground Vivid color]
-    putStr $ show (Card color value)
+    putStr $ "|"++show value ++"|"
     setSGR [SetColor Foreground Vivid White]
 
 {- Auxiliary Functions -}
+-- safeGetInt :: Int
+-- safeGetInt = do
+--     line <- getLine
+--     reads <$> getLine :: [(Int,String)]
+
+safeGetInt :: IO Int
+safeGetInt = do
+    n <- getInt
+    if n == []
+      then do
+        putStrLn "enter a valid Int"
+        safeGetInt
+      else return (head $ map fst n)
+
+getInt :: IO [(Int,String)]
+getInt = do
+    c <- getLine
+    return (reads c)
 
 nplicate :: Int -> [a] -> [a]
 nplicate n xs = concat $ (take n) $ repeat xs
